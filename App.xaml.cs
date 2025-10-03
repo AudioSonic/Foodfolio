2dd0bc22aef26773e84b1095a6a9bfb0c9387ba9
Alexander Stach <stach.alex@yahoo.de>
2025-10-03 15:50:39 +0200

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Foodfolio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
